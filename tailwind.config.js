dcb4d32522bd3832cc652688c6d0e6936a319c4e
Naman Garg <97289118+naman-ng@users.noreply.github.com>
2023-06-16 23:37:14 +0530

    Revert "init ⚡️"
    This reverts commit bf555b0aa886307a732b31521e59df9440ffe365.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      colors: {
-        fbg: "#15151F",
-        fbox: "#201D1B",
-        fmsg: "#171310",
-        forange: "#AB4F26",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
